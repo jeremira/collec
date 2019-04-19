50c709df23ab82fc14afd59f06113bf0c8ae54b7
jeremira <jmiraille@gmail.com>
2019-04-19 13:05:48 +0200

    create shop model

---
+++ spec/models/shop_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Shop, type: :model do
+  let(:shop) {create :shop}
+  
+  describe "validation" do
+    it "has a valid factory" do
+      expect(shop).to be_valid
+    end
+  end
+end
